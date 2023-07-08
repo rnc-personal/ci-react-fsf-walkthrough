dd353a27dff4e8f9b0782533af898871d5e8c2a3
rnc-personal <richard.challenger@protonmail.com>
2023-07-08 13:15:18 +0000

    Listpages, lots of other stuff tbh

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,10 @@
 import axios from "axios";
 
+// These are default values for axios requests
 axios.defaults.baseURL = "https://ci-full-stack-walkthrough-9fcaa4b5b32d.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+//These are for Axios Interceptors to refresh access token and posts
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
